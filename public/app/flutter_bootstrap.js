6ef770869d9bc2774eed36de5b1fdf3e1225c246
Miguel E. Cabrera <mecg1994@gmail.com>
2026-09-14 11:37:57 -0600

    appsss
    ssasdasd

--- public/app/flutter_bootstrap.js
+++ public/app/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2865223625" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "824743354" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
